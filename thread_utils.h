57553ce0b05c7df69e75cd18971def31147a96cf
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2024-02-08 22:58:27 +0100

    build and run scripts

--- thread_utils.h
+++ thread_utils.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <iostream>
 #include <atomic>
 #include <thread>
@@ -17,3 +15,24 @@ namespace Common {
 
     return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) == 0);
   }
+
+  /// Creates a thread instance, sets affinity on it, assigns it a name and
+  /// passes the function to be run on that thread as well as the arguments to the function.
+  template<typename T, typename... A>
+  inline auto createAndStartThread(int core_id, const std::string &name, T &&func, A &&... args) noexcept {
+    auto t = new std::thread([&]() {
+      if (core_id >= 0 && !setThreadCore(core_id)) {
+        std::cerr << "Failed to set core affinity for " << name << " " << pthread_self() << " to " << core_id << std::endl;
+        exit(EXIT_FAILURE);
+      }
+      std::cerr << "Set core affinity for " << name << " " << pthread_self() << " to " << core_id << std::endl;
+
+      std::forward<T>(func)((std::forward<A>(args))...);
+    });
+
+    using namespace std::literals::chrono_literals;
+    std::this_thread::sleep_for(1s);
+
+    return t;
+  }
+}
